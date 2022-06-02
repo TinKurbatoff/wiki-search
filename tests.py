31f0ff533965b2731e8fd73b71053c645e588704
Constantine Kurbatoff <6782841@gmail.com>
2022-06-02 15:39:13 -0400

    added some tests

--- tests.py
+++ tests.py
@@ -1,11 +1,30 @@
 import requests
 
+HEADERS_TESTS = {"——    NO HOST header ——": {},
+                 "—— Empty HOST header ——": {"Host": ""},
+                 "—— Wrong HOST header ——": {"Host": "foo-bar"},
+                 }
 
-HOSTS = {"city.local.host": []
-        }
+HOSTS_TESTS = {"city.local.host": [],
+
+               }
+                 
 headers = {}
+URL = "http://localhost:5000"
 
-for location in HOSTS.keys():
-    headers["HOST"] 
-    result = request.get()
+
+for header in HEADERS_TESTS.keys():
+    print(header)
+    result = requests.get(URL, headers=HEADERS_TESTS[header])
+    assert result.status_code == 400
+    assert result.json()['message'] == "Wrong host name!"
+    print("OK!")
+
+for location in HOSTS_TESTS.keys():
+    print(f"TEST: {location}")
+    headers["Host"] = location # HOSTS_TESTS[location] 
+    result = requests.get(URL, headers=headers)
+    print(dir(result))
+    print("OK!")
+
 
